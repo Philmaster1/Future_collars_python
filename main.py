af3453547d279ea260c71528a56ae96ee9935a4f
Philippe <philnels18@gmail.com>
2025-06-29 10:48:19 +0200

    init

--- main.py
+++ main.py
@@ -1,5 +1,53 @@
-from email import message
+print ("Hello world")
+message = "Hello"
+print (message)
+message = message + "world"
+print(message)
 
-print("hello")
-variable1 = 123
-print(variable1)
+a = 1.5
+b = 0.5
+print(a + b)
+print(a * b)
+print(a / b)
+
+a = 2
+b = 3
+print(a ** b)
+print (b % a)
+
+print(1 == 2)
+a = 1
+b = 2
+print(a == 1)
+print(a != b)
+
+print(bool(-1))
+print(bool(1))
+print(bool(2))
+print(bool(0))
+print(bool(0.0))
+print(bool(0.1))
+print(bool(""))
+print(bool("something"))
+print(bool(" "))
+print(bool(None))
+
+a = "string"
+print(type(a))
+
+a = "String"
+print(type(a) is string)
+print(type(a) is not int)
+
+print("1"+"2")
+print(int("1") + int("2"))
+
+print(bool(1))
+print(str(12))
+
+message = "new message"
+message = 2
+
+print("Hello" + "" +  "world")
+print("Hello" * 5)
+
